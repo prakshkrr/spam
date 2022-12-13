023f95c240e1e537a62ea5c33fe9c0415418c231
prakash <prakashshrivastava17@gmail.com>
2022-12-14 00:15:50 +0530

    add export column name

--- app/Exports/ExportUser.php
+++ app/Exports/ExportUser.php
@@ -5,9 +5,17 @@ namespace App\Exports;
 // use App\Models\User;
 use App\Models\Words;
 use Maatwebsite\Excel\Concerns\FromCollection;
+use Maatwebsite\Excel\Concerns\WithHeadings;
 
-class ExportUser implements FromCollection
+class ExportUser implements FromCollection, WithHeadings
 {
+
+    public function headings():array{
+        return[
+           'word_name',
+           'category_name' 
+        ];
+    } 
     /**
     * @return \Illuminate\Support\Collection
     */
